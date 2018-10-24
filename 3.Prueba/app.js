9f4648e670887823986a967b388d549834d775ed
Zarela Zanabria <zarelixzana@gmail.com>
2018-10-24 15:34:37 -0500

    Prueba

--- 3.Prueba/app.js
+++ 3.Prueba/app.js
@@ -1,25 +1,31 @@
 let arr = [' Brasil', 'Chile', 'México', 'Perú'];
 
-/* const contPadre = document.getElementsByClassName('container');
-const array2D = new Array(); 
-/*  */
 
 const hijos = document.querySelectorAll('div.container>button.btnHijo'); 
 
-/* paisB.addEventListener('click',function mensaje () {
-  for (let i = 0; i < arr.length; i++) {
 
-    const elemento= arr[i];
-    alert(elemento);
-  }
-  
-}); */
 
 for (const unHijo of hijos) {
   unHijo.addEventListener('click', (evt)=>{
     let hijo = evt.target;
-    console.log('se hizo click en', hijo);
-    console.log('Texto del enlace:', hijo.innerText);
+    if(hijo.innerText=='Primero'){
+      alert(arr[0])
+
+    }
+    if(hijo.innerText=='Segundo'){
+      alert(arr[1])
+
+    }
+    if(hijo.innerText=='Tercero'){
+      alert(arr[2])
+
+    }
+    if(hijo.innerText=='Cuarto'){
+      alert(arr[3])
+
+    }
+    /* console.log('se hizo click en', hijo); */
+    console.log('Texto del enlace:', hijo.innerText); 
     
   });
   
